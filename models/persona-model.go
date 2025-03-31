a2d66e286c35b656e633b713d7fefb0694196c52
Danysoftdev <danysoftdev@gmail.com>
2025-03-30 22:07:11 -0500

    Creado modelo de la entidad Persona

---
+++ models/persona-model.go
@@ -0,0 +1,13 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Persona struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Nombre    string             `bson:"nombre" json:"nombre"`
+	Apellido  string             `bson:"apellido" json:"apellido"`
+	Edad      int                `bson:"edad" json:"edad"`
+	Correo    string             `bson:"correo" json:"correo"`
+	Telefono  string             `bson:"telefono" json:"telefono"`
+	Direccion string             `bson:"direccion" json:"direccion"`
+}
