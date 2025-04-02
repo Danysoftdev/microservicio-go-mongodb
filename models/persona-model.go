f3f1b675706abdbe17010e96d7f03b660901c916
Danysoftdev <danysoftdev@gmail.com>
2025-04-01 19:06:21 -0500

    Agregado campo documento a la entidad Persona

--- models/persona-model.go
+++ models/persona-model.go
@@ -4,6 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Persona struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Documento string             `bson:"documento" json:"documento"`
 	Nombre    string             `bson:"nombre" json:"nombre"`
 	Apellido  string             `bson:"apellido" json:"apellido"`
 	Edad      int                `bson:"edad" json:"edad"`
